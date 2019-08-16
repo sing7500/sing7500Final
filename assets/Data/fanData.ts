aabd279a6c6c444dc1c577f19b3c5ddb931997fc
sing7500 <52579783+sing7500@users.noreply.github.com>
2019-08-16 13:09:26 -0400

    Add files via upload

---
+++ assets/Data/fanData.ts
@@ -0,0 +1,8 @@
+import {fan} from '../../app/fan'
+
+export const fandata:fan[]=[
+    {make:'Orient',color:'red',style:'modern'},
+    {make:'CYL',color:'green',style:'stylish'},
+    {make:'Tata',color:'blue',style:'colourful'},
+    {make:'Aishley',color:'white',style:'simple'}
+];
